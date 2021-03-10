c00f718ac25510879be6ab619b5f3216a68adbbf
Mit Patel <P2541151@my365.dmu.ac.uk>
2021-03-10 02:36:19 +0000

    added libstock

--- AdminSystem/StockDataEntry.aspx.cs
+++ AdminSystem/StockDataEntry.aspx.cs
@@ -4,11 +4,30 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using ClassLibrary;
 
 public partial class _1_DataEntry : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
+        //create a new instance of clsAddress
+        clsStock Gamestock = new clsStock();
+        //get the data from the session object 
+        Gamestock = (clsStock)Session["AnAddress"];
+        //display the game number on page
+        Response.Write(Gamestock.GameNumber);
 
+    }
+
+    protected void btnOK_Click(object sender, EventArgs e)
+    {
+        //creat a new instance of clsAddress
+        clsStock GameStock = new clsStock();
+        //capture the game number 
+        GameStock.GameNumber = txtGameNo.Text;
+        //store the address in the session  object
+        Session["GameStock"] =GameStock;
+        //navigate to the viewer page 
+        Response.Redirect("AddressBookViewer.aspx");
     }
 }
